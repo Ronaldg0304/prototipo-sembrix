93cbb00b1eee8ee09f1859e763f3bdf64f319bb2
Ronald Guerra <ronaldg0304@gmail.com>
2026-01-07 23:29:14 -0500

    carga inicial backend

---
+++ src/main/java/com/sena/sembrix/SembrixApplication.java
@@ -0,0 +1,13 @@
+package com.sena.sembrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SembrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SembrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/sembrix/SembrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.sembrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SembrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
